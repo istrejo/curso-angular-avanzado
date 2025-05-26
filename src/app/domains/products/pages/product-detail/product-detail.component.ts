d22a820bec60adedbc237440a3e902cb34b4531c
istrejo <istrejo2106@gmail.com>
2025-05-25 22:50:04 -0400

    refactor: update product component to use output decorator and improve product detail logic with linked signals

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,11 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  OnInit,
+  input,
+  linkedSignal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
@@ -10,11 +17,19 @@ import { CartService } from '@shared/services/cart.service';
   templateUrl: './product-detail.component.html',
 })
 export default class ProductDetailComponent implements OnInit {
-  readonly slug = input<string>();
-  product = signal<Product | null>(null);
-  cover = signal('');
   private productService = inject(ProductService);
   private cartService = inject(CartService);
+  readonly slug = input<string>();
+  $product = signal<Product | null>(null);
+  $cover = linkedSignal({
+    source: this.$product,
+    computation: (product, previousValue) => {
+      if (!product || product.images.length === 0) {
+        return previousValue;
+      }
+      return product.images[0];
+    },
+  });
 
   ngOnInit() {
     const slug = this.slug();
@@ -22,21 +37,18 @@ export default class ProductDetailComponent implements OnInit {
       this.productService.getOneBySlug(slug).subscribe({
         next: (product) => {
           console.log(product);
-          this.product.set(product);
-          if (product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
+          this.$product.set(product);
         },
       });
     }
   }
 
   changeCover(newImg: string) {
-    this.cover.set(newImg);
+    this.$cover.set(newImg);
   }
 
   addToCart() {
-    const product = this.product();
+    const product = this.$product();
     if (product) {
       this.cartService.addToCart(product);
     }
